081b3cdf1d2397c9f756ed25b6e54394753ee781
MindInitiatives <stevyn258@yahoo.com>
2019-06-24 22:02:47 +0100

    added order management options and notification manager

--- app/src/main/java/com/mindinitiatives/shoplasenaserver/OrderStatus.java
+++ app/src/main/java/com/mindinitiatives/shoplasenaserver/OrderStatus.java
@@ -81,9 +81,9 @@ public class OrderStatus extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if (item.getIntent().equals(Common.UPDATE))
+        if (item.getTitle().equals(Common.UPDATE))
             showUpdateDialog(adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));
-        else if (item.getIntent().equals(Common.DELETE))
+        else if (item.getTitle().equals(Common.DELETE))
             deleteOrder(adapter.getRef(item.getOrder()).getKey());
         return super.onContextItemSelected(item);
     }
